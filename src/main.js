daa3c5ecb48e6bb05616598c3584ec420b7184f2
Stefan Schwierz <st3sch@gmail.com>
2021-04-04 20:22:23 +0000

    Request server status

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js")
+const fetch = require("node-fetch")
 
 const client = new Discord.Client()
 
@@ -7,9 +8,11 @@ client.on("ready", () => {
 })
 
 client.on("message", msg => {
-  if (msg.content === "ping") {
-    msg.reply("pong");
-  }
+  if (msg.content === "status") {
+    fetch(process.env.STATUS_URL)
+    .then(res => res.text())
+    .then(body => msg.reply(body))
+   }
 })
 
 client.login(process.env.TOKEN)
