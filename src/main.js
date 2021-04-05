abc7feddac77ebafde248f62b2f5de709ad9df31
Stefan Schwierz <st3sch@gmail.com>
2021-04-05 20:36:29 +0000

    Track user join or leave on vhm voice channel.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,21 @@
 const Discord = require("discord.js")
 const fetch = require("node-fetch")
 
+function processEnv(name){
+    const envvar = process.env[name]
+    if (envvar === undefined) { 
+        console.error ("Environment variable not set: " + name)
+        process.exit(1)    
+    }
+    return envvar
+}
+
+// 
+const valheimBotToken       = processEnv("TOKEN")
+const valheimVoiceChannelId = processEnv("VHM_VOICE_CHANNEL_ID")
+
+
+
 const client = new Discord.Client()
 
 client.on("ready", () => {
@@ -15,4 +30,17 @@ client.on("message", msg => {
    }
 })
 
-client.login(process.env.TOKEN)
+client.on("voiceStateUpdate", (oldMember, newMember) => {
+    console.log(oldMember.channelID)
+    console.log(newMember.channelID)
+
+    let hasMemberJoinedValheimVoiceChannel = (oldMember.channelID != valheimVoiceChannelId && newMember.channelID == valheimVoiceChannelId)
+    let hasMemberLeftValheimVoiceChannel = (oldMember.channelID == valheimVoiceChannelId && newMember.channelID != valheimVoiceChannelId)
+    if ( hasMemberJoinedValheimVoiceChannel || hasMemberLeftValheimVoiceChannel) {
+        console.log(hasMemberJoinedValheimVoiceChannel)
+        console.log(hasMemberLeftValheimVoiceChannel)
+        
+    }
+})
+
+client.login(valheimBotToken)
